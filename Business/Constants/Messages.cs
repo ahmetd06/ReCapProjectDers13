fdb142dd812aa766e399b160c0086ab6b7d6d4fa
ahmetd06123456789 <ahmet.dundar@gmail.com>
2021-03-04 17:07:19 +0300

    Add project files.

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Business.Constants
+{
+    public static class Messages
+    {
+
+        public static string Eklendi = "Listelendi";
+        public static string Guncellendi = "Güncellendi";
+        public static string Silindi = "Silindi";
+        public static string Listelendi = "Listelendi";
+        public static string AracKirada = "Araç Kirada";
+        public static string AracKiralandı = "Araç Kiralandı";
+        public static string AracKiralanabilir = "Araç Kiralanabilir";
+        public static string AracKiralanamadı = "Araç Kiralanamadı";
+
+        public static string CarNameInvalid = "Araç Adı Geçersiz";
+        public static string CarDailyPriceInvalid = "Araç Günlük Ücreti Geçersiz";
+        public static string CarPictureLimitiExceed = "Araç resim limiti aşıldı";
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/ReCapProjectContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class ReCapProjectContext:DbContext
+    {
+        public ReCapProjectContext()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           
+            optionsBuilder.UseSqlServer(@"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = ReCapProject; Integrated Security = SSPI;");
+          
+            // optionsBuilder.UseSqlServer(@"Server=localhost;Database=ReCapProject;User ID=sa;Password=reallyStrongPwd123;");
+
+        }
+
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Color> Colors { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Rental> Rentals { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<CarImage> CarImages { get; set; }
+    }
+}
